7dc71e50876324ba8d4cc2fc199d4bd7456c3ce8
clemado1 <clemado1@gmail.com>
2023-06-30 21:48:35 +0900

    Hello, Rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root :to => "blog#index"
+  get "/home" => "blog#index"
+  get "/test" => "blog#test"
 end
