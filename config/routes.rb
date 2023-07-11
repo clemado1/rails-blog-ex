d69b8093f1b2352cd6ed793d62c84d18d6b0769f
clemado1 <clemado1@gmail.com>
2023-07-11 16:51:11 +0900

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'calculator/main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root :to => "blog#index"
-  post "/create" => "blog#create"
-  get "/update/:postId" => "blog#update"
-  post "/update" => "blog#updateAction"
-  get "/delete/:postId" => "blog#delete"
-
-  get "/home" => "blog#index"
-  get "/test" => "blog#test"
-  post "/result" => "calculator#result"
-  get "/plus/:num1/:num2" => "calculator#plus"
 end
--- test/controllers/calculator_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CalculatorControllerTest < ActionDispatch::IntegrationTest
-  test "should get main" do
-    get calculator_main_url
-    assert_response :success
-  end
-end
