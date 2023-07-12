f41b9d5fc67505d4abb456c64a302eca4990739f
clemado1 <clemado1@gmail.com>
2023-07-12 10:32:01 +0900

    댓글 기능 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_075201) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_005040) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_075201) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "blogs"
 end
