bedfc709e7693ac46131f3d9bf070bf250799051
clemado1 <clemado1@gmail.com>
2023-07-12 09:50:31 +0900

    Scaffolding으로 만드는 게시판

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_051710) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_075201) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
